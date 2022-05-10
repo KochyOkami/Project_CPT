fe14c2d4c9db85bdbf5363601fa8560b214b669e
KochyOkami <97362574+KochyOkami@users.noreply.github.com>
2022-05-10 08:59:27 +0200

    Delete Projet ville directory

--- Projet ville/noms_villes.py
+++
@@ -1,9 +0,0 @@
-def ville():
-    import lib.random as ra
-    
-    with open("city_name/city_names.txt","r") as v:
-        NomsVilles = v.read().split("\n")
-    return NomsVilles[ra.randint(0,len(NomsVilles)-1)]
-
-        
-    
